6cc1cb1382a27873b40a1f1025743daeea82defa
Homvi <honvedoadamkj@gmail.com>
2024-04-15 12:45:20 +0200

    add content for homepage

---
+++ LanguageContent.ts
@@ -0,0 +1,9 @@
+export const content = {
+  en: {
+    homePage: {
+      title: 'The free, fun, and effective way to learn expressions!',
+      getStartedButton: 'Get Started',
+      login: 'I already have an account',
+    },
+  },
+};
