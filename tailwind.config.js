7d895e2c3c0d034b0d6b2a743e7ace4b078f92f0
Homvi <honvedoadamkj@gmail.com>
2024-04-17 12:15:09 +0200

    add daisyUI and nova font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      nova: ['Nova Square', 'sans-serif'],
+    },
     extend: {},
   },
-  plugins: [],
-}
-
+  // TODO: resolve eslint error
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['corporate'],
+  },
+};
