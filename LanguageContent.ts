13c638fd54e3b147d1d805602659c16ca125fbf3
Homvi <honvedoadamkj@gmail.com>
2024-04-17 12:14:17 +0200

    add navbar content

--- LanguageContent.ts
+++ LanguageContent.ts
@@ -5,9 +5,20 @@ interface HomePageContent {
   login: string;
 }
 
+interface navBar {
+  request: string;
+  language: string;
+  accessibility: string;
+  font: string;
+  register: string;
+  login: string;
+  logout: string;
+}
+
 // Interface for the content of each language
 interface LanguageContent {
   homePage: HomePageContent;
+  navBar: navBar;
 }
 
 // Interface for the overall content structure
@@ -24,12 +35,30 @@ export const content: MultiLanguageContent = {
       getStartedButton: 'Get Started',
       login: 'I already have an account',
     },
+    navBar: {
+      request: 'Request expression',
+      language: 'Change language',
+      accessibility: 'Accessibility',
+      font: 'Large font size',
+      register: 'Register',
+      login: 'Log in',
+      logout: 'Log out',
+    },
   },
   es: {
     homePage: {
       title: 'La forma gratuita, divertida y eficaz de aprender expresiones!',
       getStartedButton: 'Jugar',
       login: 'Ya tengo una cuenta',
+    },
+    navBar: {
+      request: 'Solicitar expresión',
+      language: 'Cambiar idioma',
+      accessibility: 'Accessibilidad',
+      font: 'Tamaño de fuente grande',
+      register: 'Registrarse',
+      login: 'Iniciar sesión',
+      logout: 'Cerrar sesión',
     },
   },
 };
