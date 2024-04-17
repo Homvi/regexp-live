7336390e5e4fa30aedf5bb16c35925e62aa9087e
Homvi <honvedoadamkj@gmail.com>
2024-04-17 12:15:51 +0200

    add navbar

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store.ts';
+import Navbar from './components/Navbar.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <Navbar />
         <App />
       </BrowserRouter>
     </Provider>
