f00908053fc93aa309c159439bc0113d2cc703c8
Homvi <honvedoadamkj@gmail.com>
2024-04-24 17:40:55 +0200

    add route to game page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ChooseLanguage from './pages/ChooseLanguage';
+import Game from './pages/Game';
 
 function App() {
   return (
@@ -23,6 +24,14 @@ function App() {
         <Route
           path="/chooseLanguage"
           element={<ChooseLanguage />}
+        />
+        <Route
+          path="/spanishExpressions"
+          element={<Game />}
+        />
+        <Route
+          path="/englishExpressions"
+          element={<Game />}
         />
       </Routes>
     </>
