fceeb1ce06d6c3dbad525791b940f7bb984cfb2c
Homvi <honvedoadamkj@gmail.com>
2024-04-24 17:41:30 +0200

    add expression type

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+type ExpressionType = {
+    id: number;
+    expression: string;
+    rightAnswer: string;
+    falseAnswerOne: string;
+    falseAnswerTwo: string;
+  };
+  
+  export default  ExpressionType;
+  
