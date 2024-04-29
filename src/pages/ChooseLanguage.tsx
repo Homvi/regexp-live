aa9b5e8eb15204cb95b0e81c0e94df94ef1676e8
Homvi <honvedoadamkj@gmail.com>
2024-04-29 12:47:33 +0200

    change game mode on click

--- src/pages/ChooseLanguage.tsx
+++ src/pages/ChooseLanguage.tsx
@@ -2,8 +2,9 @@ import es from '../assets/es.svg';
 import en from '../assets/en.svg';
 import { Link } from 'react-router-dom';
 import { LanguageCode, content } from '../../LanguageContent.ts';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../app/store.ts';
+import { GameMode, changeGameMode } from '../features/game/gameModeSlice.ts';
 
 const ChooseLanguage = () => {
   const language: LanguageCode = useSelector(
@@ -14,6 +15,8 @@ const ChooseLanguage = () => {
     (state: RootState) => state.accessibility.isFontsizeLarge
   );
 
+  const dispatch = useDispatch();
+
   return (
     <div className={isFontSizeLarge ? 'text-3xl' : 'text-2xl'}>
       <div className="min-h-screen font-nova bg-[#052138] text-white p-3 flex flex-col">
@@ -22,7 +25,10 @@ const ChooseLanguage = () => {
         </h1>
         {/* spanish card */}
         <div className="flex flex-wrap justify-center md:justify-start gap-3">
-          <Link to="/spanishExpressions">
+          <Link
+            to="/game"
+            onClick={() => dispatch(changeGameMode(GameMode.SpanishToEnglish))}
+          >
             <div className="border-2 border-[#4c4c4c33] flex w-44 p-5 text-center cursor-pointer rounded-xl flex-col hover:border-white/40 transition-all duration-100 hover:bg-white/10 items-center">
               <img
                 src={es}
@@ -35,7 +41,10 @@ const ChooseLanguage = () => {
             </div>
           </Link>
           {/* English card */}
-          <Link to="/englishExpressions">
+          <Link
+            to="/game"
+            onClick={() => dispatch(changeGameMode(GameMode.EnglishToSpanish))}
+          >
             <div className="border-2 border-[#4c4c4c33] hover:border-white/40 transition-all duration-100 hover:bg-white/10 flex w-44 p-5 text-center cursor-pointer rounded-xl flex-col items-center">
               <img
                 src={en}
